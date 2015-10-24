27bcbccbcca3cf1e8b9c34802d02ba4a439e5e5c
Mike7R <mice7r@gmail.com>
2015-10-24 18:48:57 +0000

    GeneradorEstatsHC1 compactat

--- src/GeneradorEstatsHC1.java
+++ src/GeneradorEstatsHC1.java
@@ -1,3 +1,4 @@
+
 import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
 import java.util.ArrayList;
@@ -22,44 +23,29 @@ public class GeneradorEstatsHC1 implements SuccessorFunction {
 		//Operado 1 (Entregar bicicletes)
 		for (int i = 0; i < Main.nfurgos; ++i)
 		{
-			// la furgo esta buida
-			if (estat.furgos[i].pot_entregar())
+			for (int j = 0; j < Main.nestacions; ++j)
 			{
-				for (int j = 0; j < Main.nestacions; ++j)
+				// nomes deixem bicis a les estacions que falten
+				if (estat.furgos[i].pot_entregar() && estacions[j] < 0)
 				{
-					// nomes deixem bicis a les estacions que falten
-					if (estacions[j] < 0)
-					{
-						Estat nouestat = estat.copia();
-						// deixem el minim entre les que tenim i les que fan falta
-						int bicis = -(Math.min(nouestat.furgos[i].getbicicletes(),
-								-estacions[j]));
-						nouestat.furgos[i].enviar(j, bicis);
-						nouestat.furgos[i].canonizar();
-						R.add(new Successor("E E:" + j + " F:" + i
-								+ " Q:" + bicis, nouestat));
-					}
+					Estat nouestat = estat.copia();
+					// deixem el minim entre les que tenim i les que fan falta
+					int bicis = -(Math.min(nouestat.furgos[i].getbicicletes(),
+							-estacions[j]));
+					nouestat.furgos[i].enviar(j, bicis);
+					nouestat.furgos[i].canonizar();
+					R.add(new Successor("E E:" + j + " F:" + i
+							+ " Q:" + bicis, nouestat));
+				} else if (estat.furgos[i].pot_recollir() && estacions[j] > 0)
+				{
+					Estat nouestat = estat.copia();
+					// agafem el min entre el que podem portar i el que sobra.
+					int bicis = Math.min(Furgo.MAX, estacions[j]);
+					nouestat.furgos[i].enviar(j, bicis);
+					R.add(new Successor("R E:" + j + " F:" + i
+							+ " Q:" + bicis, nouestat));
 				}
 			}
-		}
-
-		// Operador 2 ( Recollir bicicletes )
-		for ( int i = 0; i < Main.nfurgos; ++i)
-		{
-			if ( estat.furgos[i].pot_recollir() ) {
-				for (int j = 0; j < Main.nestacions; ++j) {
-					// Nomes recollim bicicletes d'on sobren
-					if (estacions[j] > 0) {
-						Estat nouestat = estat.copia();
-						// agafem el min entre el que podem portar i el que sobra.
-						int bicis = Math.min(Furgo.MAX, estacions[j]);
-						nouestat.furgos[i].enviar(j,bicis);
-						R.add(new Successor("R E:" + j + " F:" + i +
-								" Q:" + bicis, nouestat));
-					}
-				}
-			}
-
 		}
 
 		return R;
